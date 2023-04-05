3523a281fc90d821494265e2b6793816687c1935
HGanyu <67468489+pth3231@users.noreply.github.com>
2023-04-05 14:00:49 +0000

    Fixed: int8_t insufficient mem to save neg digit

--- libs/bignum.h
+++ libs/bignum.h
@@ -12,19 +12,22 @@
 #include <string>
 
 class BigNum {
+private:
     std::string _num_A;
 
 public:
     /*
-        Constructors
+        CONSTRUCTOR
 
-        BigNum() {}
-        -> A default constructor
-        BigNum(std::string)
-        -> Copy std::string to the local std::string _num_A
-        BigNum(int32_)
-
-        Format
+        1. BigNum() {}
+            - A default constructor
+            - Time complexity: O(1)
+        2. BigNum(std::string)
+            - Copy std::string to the local std::string _num_A
+            - Time complexity: O(1)
+        3. BigNum(int64_t)
+            - Copy every digits of the numbers and assigns it to std::string _num_A
+            - Time complexity: O(logn) (logarithm base 10) 
     */
     BigNum() {}
     BigNum(std::string _init_num)
@@ -33,18 +36,74 @@ public:
     }
     BigNum(int64_t _i64_num) 
     {
+        // Emptying string
+        this->_num_A = "";
+        
+        // Specify the sign of that number and choosing the _factor
+        int16_t _factor = ((_i64_num % 10) < 0) ? -1 : 1;
+
+        // Iterate until _i64_num == 0
         while (_i64_num != 0)
         {
-            int8_t __digit = _i64_num % 10;
-            this->_num_A = char(__digit + (int8_t)(48)) + this->_num_A;
-            _i64_num /= 10;
+            int16_t _digit = _i64_num % 10 * _factor;           // Split the first digit of _i64_num
+            this->_num_A = char(_digit + 48) + this->_num_A;    // Convert and concatnate to the std::string _num_A
+            _i64_num /= 10;                                     // Remove current digit of _i64_num
         }
-    }
 
+        // Adding the sign '-' based on _factor's value
+        if (_factor == -1)
+            this->_num_A = '-' + this->_num_A;
+    }
+    BigNum(double _db_num)
+    {
+        this->_num_A = std::to_string(_db_num);
+    }
+    /*
+        BASIC GET/SET
+
+        1. std::string get()
+            - Return back current value of std::string _num_A
+            - Time complexity: O(1)
+        2. void set(std::string)
+            - Change std::string _num_A by copying the value of std::string _set_num to std::string _num_A
+            - Time complexity: O(1)
+        3. void set(int64_t)
+            - Change std::string _num_A by spliting, changing and concatnate back to std::string _num_A
+            - Time complexity: O(logn) (logarithm base 10)
+    */
     std::string get()
     {
         return this->_num_A;
     }
+    void        set(std::string _set_num)
+    {
+        this->_num_A = _set_num;
+    }
+    void        set(int64_t _i64_num)
+    {
+        // Emptying string
+        this->_num_A = "";
+        
+        // Specify the sign of that number and choosing the _factor
+        int16_t _factor = ((_i64_num % 10) < 0) ? -1 : 1;
+
+        // Iterate until _i64_num == 0
+        while (_i64_num != 0)
+        {
+            int16_t _digit = _i64_num % 10 * _factor;           // Split the first digit of _i64_num
+            this->_num_A = char(_digit + 48) + this->_num_A;    // Convert and concatnate to the std::string _num_A
+            _i64_num /= 10;                                     // Remove current digit of _i64_num
+        }
+
+        // Adding the sign '-' based on _factor's value
+        if (_factor == -1)
+            this->_num_A = '-' + this->_num_A;
+    }
+    void        set(double _db_num)
+    {
+        this->_num_A = std::to_string(_db_num);
+    }
+
 
     ~BigNum() {}
 };
--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
 #include "../libs/bignum.h"
 
 int main()
 {
     BigNum n(100000000000001);
+    std::cout << n.get() << std::endl;
+    n.set((int64_t) 1000);
+    std::cout << n.get() << std::endl;
+    n.set("-232333");
+    std::cout << n.get() << std::endl;
+    n.set(-1000.);
     std::cout << n.get() << std::endl;
     return 0;
 }
